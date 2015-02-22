6b18c64158ce9b6290a489d04664c2c3d10c5602
Tim Dep <titidep001@hotmail.com>
2015-02-22 16:17:49 +0100

    début d'affichage des articles

--- app/src/main/java/com/innervision/timtac/foodlidays/FoodCard.java
+++ app/src/main/java/com/innervision/timtac/foodlidays/FoodCard.java
@@ -13,17 +13,22 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
-
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class FoodCard extends Activity implements AdapterView.OnItemSelectedListener {
 
+    private Menu mMenu;
     private String result;
+    public static String[] string_cat = {"Boissons","Burgers et sandwich","Desserts","Sushis et makis","Salades","Pizza","Végétarien","Pâtes","Asiatique","Déjeuner","Libanais","Appéritif"};
     public ArrayList<String> cat = new ArrayList<>();
+    public static JSONArray jArray;
+    public static JSONObject jsonObject;
+    public static ListView myList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +37,15 @@ public class FoodCard extends Activity implements AdapterView.OnItemSelectedList
         setContentView(R.layout.activity_food_card);
 
         final ProgressBar myProgress = (ProgressBar)findViewById(R.id.progress);
-        ListView mylist = (ListView)findViewById(R.id.list);
+        myList = (ListView)findViewById(R.id.list);
 
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
+        cat.add("Toutes");
 
-        String url = "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/1050";
-        //String url = "http://192.168.1.13:8000/api/v1/food/cat/all/1050";
+
+        String zip_code_temp = "1435";
+        String url = "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/"+zip_code_temp;
 
         try {
             result = new GetRequest().execute(url).get();
@@ -46,48 +53,49 @@ public class FoodCard extends Activity implements AdapterView.OnItemSelectedList
             e.printStackTrace();
         }
 
-
         if(result != null)
         {
-            Toast.makeText(getApplicationContext(),"res : "+ result,Toast.LENGTH_SHORT).show();
-
             try {
 
-                JSONObject json_object = new JSONObject(result);
+                jArray = new JSONArray(result);
 
+                for(int i=0;i<jArray.length();i++) {
+                    jsonObject = jArray.getJSONObject(i);
+
+                    if (!cat.contains(string_cat[jsonObject.getInt("category_id")-1]))
+                        cat.add(string_cat[jsonObject.getInt("category_id")-1]);
+                }
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-
-            cat.add("entrées");
-            cat.add("plats");
-
             ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,cat);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(adapter);
 
-
         }
-        else Toast.makeText(getApplicationContext(),"Pas d'accès à internet", Toast.LENGTH_LONG).show();
-
+        else Toast.makeText(getApplicationContext(),"Pas de connexion internet", Toast.LENGTH_LONG).show();
 
         myProgress.setVisibility(View.GONE);
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-        return true;
+
+        mMenu = menu;
+        return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings:
+                //TODO changer les icones ?
                 Intent intent = new Intent(FoodCard.this, Settings.class);
                 startActivity(intent);
                 return true;
@@ -104,23 +112,66 @@ public class FoodCard extends Activity implements AdapterView.OnItemSelectedList
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 
         String item = parent.getItemAtPosition(pos).toString();
 
-        //Toast.makeText(getApplicationContext(),"sélectionné : " + item,Toast.LENGTH_SHORT).show();
-
-
-        //TODO mettre dans la listView tout les articles de cette catégorie et mettre un listener dessu
+        Toast.makeText(getApplicationContext(),"sélectionné : " + item,Toast.LENGTH_SHORT).show();
 
 
 
+        //TODO afficher les articles correspondants
+
+
+
+
+
+
+
+
+
+        ArrayAdapter<String> adapt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,string_cat);
+        myList.setAdapter(adapt);
+
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String article = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(getApplicationContext(),"choisi : " + article,Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
+
 
 }
